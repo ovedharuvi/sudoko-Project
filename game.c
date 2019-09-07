158eaedd48a0d81ee2b7ff0542df4a9433b33408
sher780 <41858809+sher780@users.noreply.github.com>
2019-09-07 18:39:18 +0300

    Update game.c

--- game.c
+++ game.c
@@ -4,8 +4,27 @@
 
 #include "game.h"
 
+ACTION* maintain_list(CmdType cmdType, char ** paramsArray, sudokoBoard *board,sudokoBoard *copy, ACTION* action);
 
-int load(char *path) {
+void compare_board(sudokoBoard *board, sudokoBoard *copy);
+
+ACTION* maintain_list(CmdType cmdType, char **paramsArray, sudokoBoard *board, sudokoBoard *copy, ACTION* action) {
+    switch (cmdType){
+        case (EDIT): destroyList(action); break;///////Oved
+        case (SOLVE):
+        case (SET): InsertAction(paramsArray,0,cmdType);break;
+        case (AUTOFILL || GENERATE || GUESS): compare_board(board,copy);break;
+
+
+    }
+    return FALSE;
+}
+
+void compare_board(sudokoBoard *board, sudokoBoard *copy) {
+
+}
+
+sudokoBoard * load(char *path) {
     return 0;
 }
 
@@ -13,10 +32,28 @@ int is_game_over(sudokoBoard *board) {
     return NO;
 }
 
-StatusType solve_cmd(char **paramsArray, sudokoBoard *board, MODE mode, int paramNum) {
+StatusType solve_cmd(char **paramsArray, sudokoBoard *board, MODE *p_mode, int paramNum) {
+    board = load(paramsArray[paramNum-1]);/*first and only parameter of solve function - string of the path*/
+    *p_mode = SOLVE_MODE;
+
     return FALSE;
 }
 
-StatusType initCmdArray{
+StatusType edit_cmd(char **paramsArray, sudokoBoard *board, MODE *p_mode, int paramNum) {
+    if(paramsArray[0][0] == '\0'){///////check condition, if no parameter
+        board = createBoard(DEFUALT_SIZE , DEFUALT_SIZE , FALSE);
+    }
+    else {
+        board = load(paramsArray[paramNum - 1]);/*first and only parameter of solve function - string of the path*/
+    }
+    //change mark errors to true ?
 
-};
+    *p_mode = EDIT_MODE;
+    return FALSE;
+}
+
+StatusType mark_errors_cmd(char **paramsArray, sudokoBoard *board, MODE *p_mode, int paramNum) {
+    if(paramsArray[0] != )////////////////////continue here
+    return FALSE;
+}
+
