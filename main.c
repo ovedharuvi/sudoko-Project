9d355066bdc829f9ba94f4f9d5230aba867a6448
sher780 <41858809+sher780@users.noreply.github.com>
2019-09-09 18:06:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
+
 #include "new_parser.h"
 
 
 
 
 int main() {
-    int game_stat = TRUE, prog_stat= TRUE;
+    StatusType game_stat , prog_stat;
     sudokoBoard** p_board = NULL;
     MODE *mode = NULL;
     *mode = INIT_MODE;
+    prog_stat= TRUE;
     while (prog_stat!=EXIT) {/*program is not over*/
         welcome();
-        while (game_stat!= FALSE){
-            game_stat = order(p_board,*mode);
+        game_stat = FALSE;
+        while (game_stat == FALSE){
+            game_stat = order(p_board,mode);
             if (game_stat==EXIT){
                 prog_stat = EXIT;
             }
         }
+
     }
     exit_program();/*function that releases everything*/
     return 0;
