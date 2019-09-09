7e38ef270dd4b29b1d7d88a06e3b741eb3f1ec24
sher780 <41858809+sher780@users.noreply.github.com>
2019-09-09 18:07:26 +0300

    Update new_parser.h

--- new_parser.h
+++ new_parser.h
@@ -4,21 +4,12 @@
 
 #ifndef SUDOKOPROJECT_NEW_PARSER_H
 #define SUDOKOPROJECT_NEW_PARSER_H
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
+
+#endif //SUDOKOPROJECT_NEW_PARSER_H
 #include "game.h"
 
 
-typedef enum{line_limit = 1, invalid_cmd = 2, invalid_mode = 3, invalid_param_num = 4, invalid_param_type = 5 }ErrorType;
 
-
-#define BUFFERSIZE 256
-#define MAX_PARAM 4
-#define TEXT_SIZE 25
-#define ORDERS_NUM 17
 
 void welcome();
-StatusType order(sudokoBoard **board, MODE mode);/* returns TRUE if the game is solved  successfully, FALSE otherwise, EXIT to finish the program*/
-
-#endif //SUDOKOPROJECT_NEW_PARSER_H
+StatusType order(sudokoBoard **board, MODE *p_mode);/* returns TRUE if the game is solved  successfully, FALSE otherwise, EXIT to finish the program*/
