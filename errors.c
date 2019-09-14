5201f6c4cd314665a978442a17d9202e96c786ec
sher780 <41858809+sher780@users.noreply.github.com>
2019-09-14 21:46:07 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -5,6 +5,7 @@
 StatusType error_message(ErrorType errorType, CmdInfo cmdInfo) {
     char str[TEXT_SIZE];
     char * ptoken = str;
+
     switch(errorType){
         case line_limit : printf("Error.You have entered over 256 characters.");break;
         case invalid_cmd : printf("Error. Invalid Command.");break;
@@ -13,23 +14,39 @@ StatusType error_message(ErrorType errorType, CmdInfo cmdInfo) {
                 case SOLVE_MODE: ptoken = "solve mode";break;
                 case EDIT_MODE: ptoken = "edit mode"; break;
                 case INIT_MODE: break;
-                default: ptoken = "edit and solve modes";
+                default: ptoken = "edit and solve modes"; break;
             }
-
             printf("Error.Invalid MODE for This Command. The Command %s is available on %s only.",cmdInfo.cmdName,ptoken);
             break;
-        case invalid_param_num : printf("Error.Invalid Numbers of Parameters for This Command.The Command %s needs %d Parameters."
-                    ,cmdInfo.cmdName,cmdInfo.paramNum);break;
+        case invalid_param_num :
+            printf("Error.Invalid Numbers of Parameters for This Command.The Command %s needs %d Parameters."
+                    ,cmdInfo.cmdName,cmdInfo.paramNum);
+            break;
         case invalid_param_type :
             switch (cmdInfo.paramType){
                 case Integer: ptoken = "Integer"; break;
                 case String: ptoken = "String"; break;
                 case Float: ptoken = "Float"; break;
                 case None: break;
+                default:break;
             }
 
             printf("Error.Invalid Type of Parameters for This Command.The Command %s needs Parameters in Type of %s."
-                    ,cmdInfo.cmdName,ptoken);break;
+                    ,cmdInfo.cmdName,ptoken);
+            break;
+        case board_errorneus:
+            printf("Error.Invalid board for This Command.The Command %s needs board without errors.",cmdInfo.cmdName ) ;
+            break;
+        /*continue*/
+        case incorrect_range:break;
+        case illegal_value_for_board:break;
+        case cmd_faild:break;
+        case fixed_cell:break;
+        case non_empty_cell:break;
+        case unsolvable_board:break;
+        case invalid_move:break;
+        case no_moves_error:break;
+        default:break;
     }
 
     return FALSE;
