9ffe88ac105f168322996e3271f61912ee3147de
sher780 <41858809+sher780@users.noreply.github.com>
2019-09-14 21:45:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,20 +6,20 @@
 
 int main() {
     StatusType game_stat , prog_stat;
-    sudokoBoard** p_board = NULL;
-    MODE *mode = NULL;
-    *mode = INIT_MODE;
-    prog_stat= TRUE;
+    sudokoBoard* p_board = NULL;
+    MODE mode;
+    prog_stat = TRUE;
+
     while (prog_stat!=EXIT) {/*program is not over*/
+        mode = INIT_MODE;
         welcome();
         game_stat = FALSE;
         while (game_stat == FALSE){
-            game_stat = order(p_board,mode);
+            game_stat = order(&p_board,&mode);
             if (game_stat==EXIT){
                 prog_stat = EXIT;
             }
         }
-
     }
     exit_program();/*function that releases everything*/
     return 0;
